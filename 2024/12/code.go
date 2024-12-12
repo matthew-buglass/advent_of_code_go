ea57cd67d55696a8853d19965f25c8bb5cb6663e
matthew-buglass <buglassmatthew@gmail.com>
2024-12-12 10:19:59 -0600

    added some functions and data structures for building the plot graphs.

--- 2024/12/code.go
+++ 2024/12/code.go
@@ -1,11 +1,79 @@
 package main
 
 import (
+	"regexp"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
 func main() {
 	aoc.Harness(run)
+}
+
+// General Functions
+func getLeadingIndices(matchPairs [][]int) []int {
+	if matchPairs == nil {
+		return nil
+	}
+	firstIndices := make([]int, 0, len(matchPairs))
+	for _, matchPair := range matchPairs {
+		firstIndices = append(firstIndices, matchPair[0])
+	}
+	return firstIndices
+}
+
+func parseInput(input string) (plotRuneToLocations map[string][][]int) {
+	rows := strings.Split(input, "\n")
+	uniqueCharRe := regexp.MustCompile(`.`)
+	spaceBounds := []int{len(rows) - 1, len(rows[0]) - 1}
+	strippedInput := strings.ReplaceAll(input, "\n", "")
+
+	plotRunes := uniqueCharRe.FindAllString(strippedInput, -1)
+	plotRunesIdx := getLeadingIndices(uniqueCharRe.FindAllStringSubmatchIndex(strippedInput, -1))
+
+	plotRuneToLocations = make(map[string][][]int, 0)
+	for i, rune := range plotRunes {
+		offset := plotRunesIdx[i]
+		antennaLocation := []int{offset / (spaceBounds[1] + 1), offset % (spaceBounds[1] + 1)}
+		plotRuneToLocations[rune] = append(plotRuneToLocations[rune], antennaLocation)
+	}
+
+	return plotRuneToLocations
+}
+
+// Data structures and methods
+type GardenPlot struct {
+	gardenRune    string
+	adjacentPlots []*GardenPlot
+	perimeter     int
+	area          int
+}
+
+type GardenRegion struct {
+	gardenRune  string
+	gardenPlots []*GardenPlot
+	perimeter   int
+	area        int
+}
+
+func (p *GardenPlot) calculatePerimeter() {
+	p.perimeter = 4 - len(p.adjacentPlots)
+}
+
+func (p *GardenPlot) calculateArea() {
+	p.area = 1
+}
+
+func (r *GardenRegion) calculatePerimeter() {
+	r.perimeter = 0
+	for _, plot := range r.gardenPlots {
+		r.perimeter += plot.perimeter
+	}
+}
+
+func (r *GardenRegion) calculateArea() {
+	r.area = len(r.gardenPlots)
 }
 
 // on code change, run will be executed 4 times:
--- 2024/utils.go
+++ 2024/utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -37,4 +38,35 @@ func powInt(x, y int) int {
 func waitAndClose(channel chan any, wg *sync.WaitGroup) {
 	defer close(channel)
 	wg.Wait()
+}
+
+func getLeadingIndices(matchPairs [][]int) []int {
+	if matchPairs == nil {
+		return nil
+	}
+	firstIndices := make([]int, 0, len(matchPairs))
+	for _, matchPair := range matchPairs {
+		firstIndices = append(firstIndices, matchPair[0])
+	}
+	return firstIndices
+}
+
+func parseInputSymbolsAndLocations(input string) (symbolToLocation map[string][][]int, spaceBounds []int) {
+	rows := strings.Split(input, "\n")
+	spaceBounds = []int{len(rows) - 1, len(rows[0]) - 1}
+	strippedInput := strings.ReplaceAll(input, "\n", "")
+
+	antennaRe := regexp.MustCompile(`([A-z]|[0-9])`)
+
+	antennaSymbols := antennaRe.FindAllString(strippedInput, -1)
+	antennaIdx := getLeadingIndices(antennaRe.FindAllStringSubmatchIndex(strippedInput, -1))
+
+	symbolToLocation = make(map[string][][]int, 0)
+	for i, symbol := range antennaSymbols {
+		offset := antennaIdx[i]
+		antennaLocation := []int{offset / (spaceBounds[1] + 1), offset % (spaceBounds[1] + 1)}
+		symbolToLocation[symbol] = append(symbolToLocation[symbol], antennaLocation)
+	}
+
+	return symbolToLocation, spaceBounds
 }
